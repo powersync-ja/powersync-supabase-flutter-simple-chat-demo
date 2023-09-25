e9209ad5546aa806437a6a81d9bef70cc9d64950
Kobie Botha <kobie@journeyapps.com>
2023-09-25 14:25:57 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# supabase-tutorial-chat-app
+Template repo based on the Supabase Flutter tutorial to build a simple chat app: https://supabase.com/blog/flutter-tutorial-building-a-chat-app
---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import './utils/constants.dart';
+import './pages/splash_page.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await dotenv.load(fileName: '.env');
+
+  await Supabase.initialize(
+    url: dotenv.env['SUPABASE_URL']!,
+    anonKey: dotenv.env['SUPABASE_ANON_KEY']!,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'My Chat App',
+      theme: appTheme,
+      home: const SplashPage(),
+    );
+  }
+}
